b5ac87f718f4440219fedaa77c55eb188405202f
Blazyst <blazyst8@gmail.com>
2026-01-23 09:36:04 +0100

    timer + button  adds times

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -3,13 +3,31 @@ using System;
 
 public partial class Main : Node2D
 {
+	private Timer timer;
+
+	private Button btn;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this.timer = new Timer();
+		AddChild(timer);
+		timer.WaitTime = 150;
+		timer.Start();
+		this.btn = GetNode<Button>("Button");
+		this.btn.ButtonDown += OnButtonDown;
+	}
+
+	private void OnButtonDown()
+	{
+		double time = timer.TimeLeft;
+		timer.Stop();
+		timer.WaitTime = time += 1.0;
+		timer.Start();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GetNode<Label>("Label").Text = timer.TimeLeft.ToString("F0");
 	}
 }
